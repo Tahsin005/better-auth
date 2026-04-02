6bf6438bf4178b07425ab93e8ff26d70a8d696aa
tahsin005 <tahsin.ferdous3546@gmail.com>
2026-04-02 20:37:12 +0600

    feat: initial commit

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <div className="my-6 px-4 max-w-md mx-auto">
+      <div className="text-center space-y-6">
+        <h1 className="text-3xl font-bold">Welcome to Our App</h1>
+        <Link href={'/auth/login'}>Sign In</Link>
+      </div>
+    </div>
+  );
+}
