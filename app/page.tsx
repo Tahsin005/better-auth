1fd6984466385f39b0dea592a45f08a7c6a721b7
tahsin005 <tahsin.ferdous3546@gmail.com>
2026-04-06 20:16:24 +0600

    feat: implement social authentication with GitHub and Discord, add reusable action buttons

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { BetterAuthActionButton } from "@/components/auth/better-auth-action-buttion";
 import { Button } from "@/components/ui/button";
 import { authClient } from "@/lib/auth-client";
 import Link from "next/link";
@@ -28,10 +29,9 @@ export default function Home() {
         ): (
           <>
             <h1 className="text-3xl font-bold">Welcome {session.user.name}</h1>
-            {/* TODO: add loading state */}
-            <Button size="lg" variant="destructive" onClick={() => authClient.signOut()}>
+            <BetterAuthActionButton size="lg" variant="destructive" action={() => authClient.signOut()}>
               Sign Out
-            </Button>
+            </BetterAuthActionButton>
           </>
         )}
       </div>
---
+++ lib/o-auth-providers.ts
@@ -0,0 +1,14 @@
+import { DiscordIcon, GitHubIcon } from "@/components/auth/o-auth-icons";
+import { ComponentProps, ElementType } from "react";
+
+export const SUPPORTED_OAUTH_PROVIDERS = ["github", "discord"] as const;
+
+export type SupportedOAuthProvider = (typeof SUPPORTED_OAUTH_PROVIDERS)[number];
+
+export const SUPPORTED_OAUTH_PROVIDER_DETAILS: Record<SupportedOAuthProvider, {
+    name: string;
+    Icon: ElementType<ComponentProps<"svg">>;
+}> = {
+    github: { name: "Github", Icon: GitHubIcon },
+    discord: { name: "Discord", Icon: DiscordIcon },
+}
